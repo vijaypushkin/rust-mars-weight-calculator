96a18ed09af36afe2d75fbb8a1c3fca733329af4
Vijay Pushkin <mail@vijaypushkin.me>
2022-08-30 19:39:46 +0530

    Added formula

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,7 @@
 fn main() {
-    println!("Hello, world!");
+    println!("Weight on Mars is {}kg", calculate_weight_on_mars(68.0));
+}
+
+fn calculate_weight_on_mars(weigth: f32) -> f32 {
+    (weigth / 9.81) * 3.711
 }
