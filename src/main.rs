794e3d4f930243060b5d563878cd1e63897217a4
Vijay Pushkin <mail@vijaypushkin.me>
2022-09-10 18:48:00 +0530

    Taking input from user and returning it

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Weight on Mars is {}kg", calculate_weight_on_mars(68.0));
+    let mut input = String::new();
+
+    println!("Enter your weight in kg: ");
+
+    io::stdin().read_line(&mut input).unwrap();
+
+    let weight: f32 = input.trim().parse().unwrap();
+
+    println!("Weight on Mars is {}kg", calculate_weight_on_mars(weight));
 }
 
 fn calculate_weight_on_mars(weigth: f32) -> f32 {
